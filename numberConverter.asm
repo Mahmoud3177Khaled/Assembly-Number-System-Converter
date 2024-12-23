f880313205872b96b4af37c7a41586111290df28
philokaram <philokrm@gmail.com>
2024-12-23 20:19:29 +0200

    Update numberConverter.asm

--- numberConverter.asm
+++ numberConverter.asm
@@ -1,5 +1,5 @@
 .data
-	# any data
+	RANGE_NUMBERS: .byte 1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'
 .text 
 	main:
 		#read the input
@@ -14,7 +14,8 @@
 		
 		# DecimalToOther function call
 		
-		
+		jal decimal_to_other_function
 		
 		# write Output
 	
+decimal_to_other_function:
