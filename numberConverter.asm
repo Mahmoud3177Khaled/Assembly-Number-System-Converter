c1a0afd2c34e6d29936817ac88fce8ff35916116
philokaram <philokrm@gmail.com>
2024-12-23 18:53:26 +0200

    add empty files

---
+++ numberConverter.asm
@@ -0,0 +1,20 @@
+.data
+	# any data
+.text 
+	main:
+		#read the input
+		
+		
+		
+		# validation function call
+		
+		
+		# OtherToDecimal function call
+		
+		
+		# DecimalToOther function call
+		
+		
+		
+		# write Output
+	
