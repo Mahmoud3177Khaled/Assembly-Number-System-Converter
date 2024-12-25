0381e67b4540770be3144c9885df9658680c92ac
Mahmoud Khaled <124203317+Mahmoud3177Khaled@users.noreply.github.com>
2024-12-25 22:12:45 +0200

    added other to decimal and linked with decimal to other

--- numberConverter.asm
+++ numberConverter.asm
@@ -6,6 +6,8 @@
 	error_message: .asciiz "Error: Number doesn't belong to the current system.\n"
 	RANGE_NUMBERS: .byte '0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'
 	
+	number_to_decimal: .space 50
+	base_to_decimal: .word 10
 	#current_system: .word 0
 	#number: .word 0
 	#new_system: .word 0
@@ -18,22 +20,21 @@
     		la $a0, prompt_current_system
     		syscall
 
-    		#read current system
-    		li $v0, 5
-    		syscall
-    		#sw $v0, current_system
-		move $t0,$v0            # $t0 --> cureent system 
+    		# read the base of the number to conver to decimal
+		li $v0, 5
+		syscall
+		sw $v0, base_to_decimal    
 	
    		#prompt for number
     		li $v0, 4
     		la $a0, prompt_number
     		syscall
 
-    		#read number
-    		li $v0, 5
-    		syscall
-    		#sw $v0, number
-		move $t1,$v0            # $t1 --> number 
+    		# read the number to convert to decimal
+		li $v0, 8
+		la $a0, number_to_decimal
+		li $a1, 50
+		syscall            
 		
 	
    		#prompt for new system
@@ -45,29 +46,25 @@
    		li $v0, 5
     		syscall
     		#sw $v0, new_system
-    		move $t2,$v0            # $t2 --> new system 
+    		move $t8,$v0            # $t8 --> new system 
 
 		# 2- validation function call
     		# Assuming validate_number belongs in current system: other function call placeholder
     		# Example usage: jal validate_number (this would be part of the extended implementation)
 
-		# 3- otherToDecimal function call
-		
-		
 
-		# Convert other to decimal: call OtherToDecimal function
-    		# Example: 
-    		 lw $a0, number
-    		 lw $a1, current_system
+		# 3- otherToDecimal function call
+    		 jal other_to_decimal_function
     		 
-    		 jal OtherToDecimal
-  
+    		 
   		# 4- decimalToOther function call
 		
-    		move $a0,$t1    #- some output from OtherToDecimal -
-    		move $a1, $t2
+    		move $a0,$t2    #- some output from OtherToDecimal -
+    		move $a1, $t8
     		li $s0 , 0 # $s0 --> count number in the stack
 		jal decimal_to_other_function
+		
+		
 
 		# 5- write Output
     		li $v0, 4
@@ -87,7 +84,86 @@
    		exit:
     		li $v0, 10
     		syscall
-
+	
+	other_to_decimal_function:
+		# get the string length
+		
+		# number start
+		la $t0, number_to_decimal
+		# counter
+		li $t1, 0
+		# char for comparison
+		lb $t2, 0($t0)
+		
+		find_length_loop:
+			beq $t2, $zero, length_found
+			addi $t1, $t1, 1
+			addi $t0, $t0, 1
+			lb $t2, 0($t0)
+			j find_length_loop
+			
+		length_found:
+		subi $t1, $t1, 2   # subt 2 to get last index (jump \0 and account for 0-based)
+		#li $v0, 1
+		#move $a0, $t1
+		#syscall
+		
+		# start conversion
+		
+		# adress of first char of number
+		la $s0, number_to_decimal
+		# base
+		lw $s1, base_to_decimal
+		# temp char to proccess
+		lb $s2, 0($s0)
+		# length of num
+		move $s3, $t1
+		# loop length (s3 + 1)
+		move $t6, $s3
+		addi $t6, $t6, 1
+		# temp location of cuurent char
+		move $s4, $s0 
+		# x to mult by each time (step: i = i*$t0)
+		li $t1, 1
+		# sum (TOTAL number in decimal)
+		li $t2, 0 
+		
+		find_in_decimal_loop:
+			move $s4, $s0
+			beq $t6, $zero, found_in_decimal
+			add $s4, $s4, $s3  # add length in s3 to s4 to get address of next char from behind
+			lb $s2, 0($s4)     # s2 has first char address
+			
+			li $t7, 65
+			bge $s2, $t7, subt_alpha
+			li $t7, 48
+			bge $s2, $t7, subt_num
+			
+			subt_alpha:
+			subi $s2, $s2, 55
+			#addi $s2, $s2, 10
+			j continue
+			
+			subt_num:
+			subi $s2, $s2, 48
+			j continue
+			
+			
+			continue: # s2 now has the degit value
+			
+			mul $s2, $s2, $t1  # mult by base^x    (x is t1)
+			add $t2, $t2, $s2  # add digit value
+			
+			subi $s3, $s3, 1   # get next char from behind
+			subi $t6, $t6, 1
+			move $s4, $s0      # reset s4 to first char address 
+			mul $t1, $t1, $s1  # multi x by base for conversion
+		
+		j find_in_decimal_loop
+		
+		
+		found_in_decimal:
+			jr $ra
 				
 	
 	decimal_to_other_function:
